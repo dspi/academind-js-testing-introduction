8fd1edf03834d809d2be8f30fe2844778adeb35a
DavidS <davids@digitalcabinet.co.za>
2023-03-10 23:24:11 +0100

    Adds an integration test, after a needed refactor.

--- util.test.js
+++ util.test.js
@@ -1,7 +1,13 @@
-const { generateText } = require('./util');
+const { generateText, checkAndGenerate } = require('./util');
 
 //UNIT TEST
 test('should output name and age', () => {
   const text = generateText('Max', 29);
+  expect(text).toBe('Max (29 years old)');
+});
+
+//INTEGRATION TEST
+test('should validate input and produce test output', () => {
+  const text = checkAndGenerate('Max', 29);
   expect(text).toBe('Max (29 years old)');
 });
