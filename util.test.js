23dc233b41c8ec3f329525c59325139bca53cece
DavidS <davids@digitalcabinet.co.za>
2023-03-10 22:48:07 +0100

    Adds a unit test.

---
+++ util.test.js
@@ -0,0 +1,7 @@
+const { generateText } = require('./util');
+
+//UNIT TEST
+test('should output name and age', () => {
+  const text = generateText('Max', 29);
+  expect(text).toBe('Max (29 years old)');
+});
