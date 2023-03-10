62fb229cd07ecdc0b932552065164796c18edf14
DavidS <davids@digitalcabinet.co.za>
2023-03-10 23:49:38 +0100

    adds Puppeteer as a dev dependency, and adds an end-to-end test.

--- util.test.js
+++ util.test.js
@@ -1,3 +1,4 @@
+const puppeteer = require('puppeteer');
 const { generateText, checkAndGenerate } = require('./util');
 
 //UNIT TEST
@@ -11,3 +12,26 @@ test('should validate input and produce test output', () => {
   const text = checkAndGenerate('Max', 29);
   expect(text).toBe('Max (29 years old)');
 });
+
+//END-TO-END TEST
+test('should create an element  with text and correct class', async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 80,
+    args: ['--window-size=400,600']
+  });
+  const page = await browser.newPage();
+  page.goto(
+    'file:///data/david/dev/00_learn/academind-js-testing-introduction/index.html'
+  );
+
+  await page.waitForNavigation();
+
+  await page.click('input#name'); //not strictly required
+  await page.type('input#name', 'Anna');
+  await page.click('input#age'); //not strictly required
+  await page.type('input#age', '28');
+  await page.click('#btnAddUser');
+  const finalText = await page.$eval('.user-item', el => el.textContent);
+  expect(finalText).toBe('Anna (28 years old)');
+}, 30000); //Add a timeout to prevent exceeeding default 5000.
